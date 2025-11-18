dc08a26178a6ca5577c5622f6bfc8b8816a5dd2d
Alan Mendes Vieira <alan.mendes@aluno.ufca.edu.br>
2025-11-18 23:21:38 +0000

    User model class implementation

--- models/User.py
+++ models/User.py
@@ -4,5 +4,24 @@ class User:
         - Métodos: getters e setters.
     """
     def __init__(self, user_id: int, name: str, email: str, password_hash: str):
-        raise NotImplementedError("Unimplemented feature.")
-        
+        self.user_id = user_id
+        self.name = name
+        self.email = email
+        self.password_hash = password_hash
+
+    @property
+    def user_id(self):
+        return self.__user_id
+    
+    @user_id.setter
+    def user_id(self, user_id):
+        # validation ? acho que não pois a camada de autenticação será responsável por garantir a validade do usuário
+        self.__user_id = user_id
+    
+    @property
+    def password_hash(self):
+        return self.__password_hash
+    
+    @password_hash.setter
+    def password_hash(self, password_hash):
+        self.__password_hash = password_hash
