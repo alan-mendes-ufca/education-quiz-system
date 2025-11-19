b3bfcd11904c9e9a07e73d0926ca27f418e00b04
Alan Mendes Vieira <alan.mendes@aluno.ufca.edu.br>
2025-11-19 00:21:39 +0000

    QuizResult model class implemmentation

--- models/QuizResult.py
+++ models/QuizResult.py
@@ -6,4 +6,8 @@ class Quiz_result():
         Define as características básicas e CONFIGURAÇÕES do quiz.
     """
     def __init__(self, user:User, quiz: Quiz, score_achieved: int, time_taken: float, responses_history:dict):
-        raise NotImplementedError("Unimplemented feature.")
+        self.user = user
+        self.quiz = quiz
+        self.score = score_achieved
+        self.time = time_taken
+        self.responses = responses_history
