5a2935c3e7ae4400544bbb1cd7715b2d21274d87
Alan Mendes Vieira <alan.mendes@aluno.ufca.edu.br>
2025-11-20 16:48:19 +0000

    MultipleChoice class implemmentation.

--- models/MultipleChoice.py
+++ models/MultipleChoice.py
@@ -5,6 +5,16 @@ class MultipleChoiceQuestion(Question):
         Responsável por guardar os dados das questões de multipla escolha.
         - Deve implementar o método check_answer()!!!!!!
     """
-    def __init__(self, options: list, correct_option_index: int):
-        super().__init__()
-        raise NotImplementedError("Unimplemented feature.")
+    def __init__(self, question_id: int = None, proposition: str = None, theme: str = None, difficulty_points: int = None, options: list = None, correct_option_index: int = None):
+        super().__init__(question_id, proposition, theme, difficulty_points)
+        self.options = options
+        self.correct_option_index = correct_option_index
+    
+    def check_answer(self, user_answer_index):
+        if user_answer_index != self.correct_option_index:
+            return "Invalid response."
+        return "Valid response."
+    
+    @classmethod
+    def constructor_dict(cls, dict):
+        return cls(dict["id"], dict["proposition"], dict["theme"], dict["difficulty_points"], dict["options"], dict["correct_option_index"])
