6d298a37f59cc84fb4e59858fdf85e4b8815b6cb
Alan Mendes Vieira <alan.mendes@aluno.ufca.edu.br>
2025-12-10 23:00:11 +0000

    Updating interface.

--- flaskr/app.py
+++ flaskr/app.py
@@ -11,7 +11,16 @@ from services.QuizGame import QuizGame
 logging.getLogger("watchdog.observers.inotify").setLevel(logging.WARNING)
 logging.getLogger("watchdog").setLevel(logging.WARNING)
 
-from flask import Flask, render_template, request, session, flash, redirect, url_for, jsonify
+from flask import (
+    Flask,
+    render_template,
+    request,
+    session,
+    flash,
+    redirect,
+    url_for,
+    jsonify,
+)
 
 from models.InvalidCredentialError import InvalidCredentialsError
 from models.Quiz import Quiz
@@ -31,6 +40,7 @@ app.secret_key = "my_local_secret_key"
 def index():
     return render_template("index.html")
 
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
@@ -47,7 +57,7 @@ def register():
         try:
             auth = AuthService()
             session["user"] = (auth.register(name, email, password)).user_id
-        
+
         except Exception as e:
             return return_error(f"Error: {e}", "/register")
 
@@ -85,36 +95,42 @@ def login():
 @login_required
 def logout():
     # Remove o usuário da sessão
-    session.pop('user', None)
-    return redirect(url_for('index'))
+    session.pop("user", None)
+    return redirect("/login")
+
 
 @app.route("/quiz/create")
 @login_required
 def create_quiz():
     return render_template("create_quiz.html")
 
+
 @app.route("/quiz/save", methods=["POST"])
 def save_quiz():
-    if request.method == "POST":        
+    if request.method == "POST":
         try:
             quiz_info = Quiz.from_dict(request.get_json())
-            
+
             quiz_repo = QuizRepository()
             quiz_repo.create(quiz_info)
         except Exception as e:
             print(f"Aconteceu um erro {e}")
 
-        return jsonify({'info': 'API funcionando'}), 200
+        return jsonify({"info": "API funcionando"}), 200
+
 
 # Catálogo de quizzes
 @app.route("/quizzes")
 def quizzes_page():
     return render_template("quizzes_list.html")
+
+
 @app.route("/api/quizzes")
 def quizzes_api():
     quiz_repo = QuizRepository()
-    list_quiz = quiz_repo.get_most_popular() # retorna uma list[quiz]
+    list_quiz = quiz_repo.get_most_popular()  # retorna uma list[quiz]
     return [d.to_dict() for d in list_quiz]
+
 
 @app.route("/api/quizzes/search")
 def get_quizzes():
@@ -122,36 +138,38 @@ def get_quizzes():
     list_quiz = quiz_repo.get_by_category(request.args.get("category"))
 
     if not list_quiz:
-        return [];
+        return []
     return [d.to_dict() for d in list_quiz]
+
 
 @app.route("/quiz/<int:quiz_id>/play", methods=["GET", "POST"])
 def quiz_init(quiz_id):
     # Inicializa o quiz
-    if request.method == "GET":    
+    if request.method == "GET":
         quiz_repo = QuizRepository
         q_game = QuizGame(quiz_repo.get_by_id(quiz_id), session["user"])
 
         # Pesistência da run
         session["quiz_session"] = {
             "user_id": session["user"],
-            "quiz_id" : quiz_id,
-            "current_question" : 0,
-            "score" : 0
+            "quiz_id": quiz_id,
+            "current_question": 0,
+            "score": 0,
         }
 
         quiz_session = QuizSessionRepository()
         quiz_session.create(QuizSession.from_dict(session["quiz_session"]))
 
         # Retornando a primeira pergunta.
-        return render_template("quiz_run.html", question=q_game.start_game()) 
-    
+        return render_template("quiz_run.html", question=q_game.start_game())
+
     # Registra respostas e mantém fluxo
-    else : 
+    else:
         # Persistência da resposta do usuário com UserAnswers
         # Atualizar sessão
         # Retornar próxima questão para o front
         pass
+
 
 """
 Rotas faltantes:
